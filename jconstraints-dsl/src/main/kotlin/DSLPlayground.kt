d210828450d4ea9a38ae1615a143feda3e428c8f
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-09-13 16:59:31 +0200

    Add some comments to DSLPlayground for demonstration purposes.

--- jconstraints-dsl/src/main/kotlin/DSLPlayground.kt
+++ jconstraints-dsl/src/main/kotlin/DSLPlayground.kt
@@ -3,14 +3,20 @@ import tools.aqua.jconstraints.solvers.portfolio.sequential.StringOrFloatExpress
 
 class DSLPlayground {
 	init {
-		val cvcseqeval = SolverCompositionDSL.sequentialSolver {
+		val cvcSeqEval = SolverCompositionDSL.sequentialSolver {
 			solver(Solver.CVC4) {
+				//specify how long the solver should run before being killed.
 				timer = Time.minutes(1)
+				//specify a name by which this solver is identified in the finalVerdict block.
 				name = "CVC4"
 				runIf {
+					//specify whether to run this solver or not.
+					//expression visiting can be done here.
 					it.accept(StringOrFloatExpressionVisitor(), null)
 				}
 				continueIf { expression, result, valuation ->
+					//specify based on the expression, result and valuation obtained from running this solver,
+					//whether to continue or stop this sequential solver composition.
 					when (result) {
 						Result.SAT -> !expression.evaluateSMT(valuation)
 						Result.UNSAT -> false
@@ -19,19 +25,22 @@ class DSLPlayground {
 				}
 			}
 			solver(Solver.Z3)
-			finalVerdict { it.lastOrNull()?.second ?: Result.DONT_KNOW }
+			//specify how a final verdict is obtained from individual solver runs.
+			finalVerdict {
+				it.lastOrNull()?.second ?: Result.DONT_KNOW
+			}
 		}
 
 		val majorityVote = SolverCompositionDSL.parallelSolver {
-			solver(Solver.Z3)
+			solver(Solver.Z3) //adds a solver
 			solver(Solver.CVC4)
-			solver(cvcseqeval) {
+			solver(cvcSeqEval) {
 				timer = Time.seconds(120)
 				runIf {
-					//hier könnte die expression untersucht werden
 					true
 				}
 			}
+			//specify number of solvers that need to return a result before stopping the parallel composition.
 			waitFor(3)
 			finalVerdict { list ->
 				list.groupBy { it.second }
